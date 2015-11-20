eafc52d28708783d8e85f357258b02d20d48dfa7
osima <blue@osima.jp>
2015-11-20 16:09:16 +0900

    fixed indent.

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,21 @@
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+		app.receivedEvent('deviceready');
 
 		var failure = function() {};
 
@@ -32,15 +32,15 @@ var app = {
 			var pageCount = parseInt( pageCountAsString );
 			for( var index=0; index<pageCount; index++ ){
 				var targetPageNumberAsString = String(index+1);
-		    	cataloghelper.process("www/catalog.pdf",targetPageNumberAsString,"297","210", itemLoaded, failure);
+				cataloghelper.process("www/catalog.pdf",targetPageNumberAsString,"297","210", itemLoaded, failure);
 			}
 	   	};
 
-	    cataloghelper.pageCount("www/catalog.pdf", showCatalog, failure);
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-    }
+		cataloghelper.pageCount("www/catalog.pdf", showCatalog, failure);
+	},
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+	}
 };
 
 app.initialize();
