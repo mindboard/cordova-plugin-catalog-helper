3ce0dd514e47ad5040b7aa7bcc206d13a582ec39
osima <blue@osima.jp>
2015-12-11 10:15:04 +0900

    fixed indent.

--- src/ios/CatalogHelper.m
+++ src/ios/CatalogHelper.m
@@ -3,110 +3,111 @@
 @implementation CatalogHelper
 
 + (CGImageRef)
-	resize	: (CGImageRef)image
-   	toWidth   : (int)width
-   	andHeight : (int)height {
+    resize    : (CGImageRef)image
+    toWidth   : (int)width
+    andHeight : (int)height {
 
-	CGColorSpaceRef colorSpace = CGImageGetColorSpace(image);
-	CGContextRef context = CGBitmapContextCreate(
-		   	NULL, width, height,
-		   	CGImageGetBitsPerComponent(image), CGImageGetBytesPerRow(image),
-		   	colorSpace, CGImageGetAlphaInfo(image));
-   	CGColorSpaceRelease(colorSpace);
-	
-	if(context == NULL){
-		return nil;
-	}
-	
-	CGContextDrawImage(context, CGRectMake(0, 0, width, height), image);
-	CGImageRef retVal = CGBitmapContextCreateImage(context);
-	CGContextRelease(context);
-	
-	return retVal;
+    CGColorSpaceRef colorSpace = CGImageGetColorSpace(image);
+    CGContextRef context = CGBitmapContextCreate(
+               NULL, width, height,
+               CGImageGetBitsPerComponent(image), CGImageGetBytesPerRow(image),
+               colorSpace, CGImageGetAlphaInfo(image));
+    CGColorSpaceRelease(colorSpace);
+    
+    if(context == NULL){
+        return nil;
+    }
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), image);
+    CGImageRef retVal = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    
+    return retVal;
 }
 
 + (CGPDFDocumentRef)
-   	pdfDocument : (NSString *) pdfFilePath {
+    pdfDocument : (NSString *) pdfFilePath {
 
-	NSString *fullPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:pdfFilePath];
-	CFURLRef url = CFURLCreateWithFileSystemPath (NULL, (CFStringRef)fullPath, kCFURLPOSIXPathStyle, 0);
-	CGPDFDocumentRef retVal = CGPDFDocumentCreateWithURL(url);
-	CFRelease(url);
+    NSString *fullPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:pdfFilePath];
+    CFURLRef url = CFURLCreateWithFileSystemPath (NULL, (CFStringRef)fullPath, kCFURLPOSIXPathStyle, 0);
+    CGPDFDocumentRef retVal = CGPDFDocumentCreateWithURL(url);
+    CFRelease(url);
 
-	return retVal;
+    return retVal;
 }
 
 + (NSString *)
-   	base64PngString	 : (NSString *)pdfFilePath
-   	pageNumber		  : (int)pageNumber
-   	pageWidth		   : (int)pageWidth
-   	pageHeight		  : (int)pageHeight {
+    base64PngString     : (NSString *)pdfFilePath
+    pageNumber          : (int)pageNumber
+    pageWidth           : (int)pageWidth
+    pageHeight          : (int)pageHeight {
 
-	CGPDFDocumentRef pdfDocument = [CatalogHelper pdfDocument:pdfFilePath];
-	CGPDFPageRef	 page		= CGPDFDocumentGetPage(pdfDocument, pageNumber);
+    CGPDFDocumentRef pdfDocument = [CatalogHelper pdfDocument:pdfFilePath];
+    CGPDFPageRef     page        = CGPDFDocumentGetPage(pdfDocument, pageNumber);
 
-	CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-	
-	CGRect mediaBoxRect = CGPDFPageGetBoxRect(page, kCGPDFMediaBox);
-	CGContextRef bitmapContext = CGBitmapContextCreate(
-		   	nil,
-		   	mediaBoxRect.size.width,
-		   	mediaBoxRect.size.height,
-		   	8, 0, colorSpace, kCGImageAlphaPremultipliedFirst);
-   	CGColorSpaceRelease(colorSpace);
-	
-	CGContextFlush(bitmapContext);
-	CGContextDrawPDFPage(bitmapContext, page);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGRect mediaBoxRect = CGPDFPageGetBoxRect(page, kCGPDFMediaBox);
+    CGContextRef bitmapContext = CGBitmapContextCreate(
+               nil,
+               mediaBoxRect.size.width,
+               mediaBoxRect.size.height,
+               8, 0, colorSpace, kCGImageAlphaPremultipliedFirst);
+    CGColorSpaceRelease(colorSpace);
+    
+    CGContextFlush(bitmapContext);
+    CGContextDrawPDFPage(bitmapContext, page);
 
-	//CGPDFPageRelease(page);
-	CGPDFDocumentRelease( pdfDocument );
+    //CGPDFPageRelease(page);
+    CGPDFDocumentRelease( pdfDocument );
 
-	CGImageRef cgImage = CGBitmapContextCreateImage(bitmapContext);
-	CGImageRef resizedCgImage = [CatalogHelper resize:cgImage toWidth:pageWidth andHeight:pageHeight];
-	CFRelease(cgImage);
+    CGImageRef cgImage = CGBitmapContextCreateImage(bitmapContext);
+    CGImageRef resizedCgImage = [CatalogHelper resize:cgImage toWidth:pageWidth andHeight:pageHeight];
+    CFRelease(cgImage);
 
-	CGContextRelease(bitmapContext);
+    CGContextRelease(bitmapContext);
 
-	UIImage *theUIImage = [UIImage imageWithCGImage:resizedCgImage];
-	NSString *encodedString = [UIImagePNGRepresentation(theUIImage) base64Encoding];
-	CFRelease(resizedCgImage);
+    UIImage *theUIImage = [UIImage imageWithCGImage:resizedCgImage];
+    NSString *encodedString = [UIImagePNGRepresentation(theUIImage) base64Encoding];
+    CFRelease(resizedCgImage);
 
-	return encodedString;
+    return encodedString;
 }
 
 - (void)
-   	process : (CDVInvokedUrlCommand *) command {
+    process : (CDVInvokedUrlCommand *) command {
 
-	NSString* callbackId		 = [command callbackId];
-	NSString* pdfFilePath		= [[command arguments] objectAtIndex:0];
-	NSString* pageNumberAsString = [[command arguments] objectAtIndex:1];
-	NSString* widthAsString	  = [[command arguments] objectAtIndex:2];
-	NSString* heightAsString	 = [[command arguments] objectAtIndex:3];
+    NSString* callbackId         = [command callbackId];
+    NSString* pdfFilePath        = [[command arguments] objectAtIndex:0];
+    NSString* pageNumberAsString = [[command arguments] objectAtIndex:1];
+    NSString* widthAsString      = [[command arguments] objectAtIndex:2];
+    NSString* heightAsString     = [[command arguments] objectAtIndex:3];
 
-	NSString *message = [CatalogHelper
-	   	base64PngString : pdfFilePath
-	   	pageNumber		: [pageNumberAsString intValue]
-	   	pageWidth		: [widthAsString intValue]
-	   	pageHeight		: [heightAsString intValue] ];
+    NSString *message = [CatalogHelper
+           base64PngString : pdfFilePath
+           pageNumber        : [pageNumberAsString intValue]
+           pageWidth        : [widthAsString intValue]
+           pageHeight        : [heightAsString intValue] ];
 
-	CDVPluginResult* result = [CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsString  : message];
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus : CDVCommandStatus_OK messageAsString  : message];
 
-	[self.commandDelegate sendPluginResult:result callbackId:callbackId];
+    [self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
 
-- (void) pageCount:(CDVInvokedUrlCommand*)command;
-{
-	NSString* callbackId  = [command callbackId];
-	NSString* pdfFilePath = [[command arguments] objectAtIndex:0];
+- (void)
+    pageCount:(CDVInvokedUrlCommand*) command {
 
-	CGPDFDocumentRef pdfDocument = [CatalogHelper pdfDocument:pdfFilePath];
-	size_t pageCount = CGPDFDocumentGetNumberOfPages(pdfDocument);
-	CGPDFDocumentRelease(pdfDocument);
+    NSString* callbackId  = [command callbackId];
+    NSString* pdfFilePath = [[command arguments] objectAtIndex:0];
 
-	NSString *message = [NSString stringWithFormat:@"%ld", pageCount];
-	CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+    CGPDFDocumentRef pdfDocument = [CatalogHelper pdfDocument:pdfFilePath];
+    size_t pageCount = CGPDFDocumentGetNumberOfPages(pdfDocument);
+    CGPDFDocumentRelease(pdfDocument);
 
-	[self.commandDelegate sendPluginResult:result callbackId:callbackId];
+    NSString *message = [NSString stringWithFormat:@"%ld", pageCount];
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+
+    [self.commandDelegate sendPluginResult:result callbackId:callbackId];
 }
 
 @end
